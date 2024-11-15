3e15e172ddc90f328350ad111d8940209ec68c8f
sireeshayanamala11 <sireesha.yanamala11@gmail.com>
2024-11-15 19:25:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('hello Sirisha!');
 });
 
 app.listen(port, () => {
-  console.log('server started on port $(port}');
+  console.log('server started on port ${port}');
 });
